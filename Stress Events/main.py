f52759d894ff651ea1bedd8685f0025604c98f95
Luke Klegraefe <lukeklegraefe@gmail.com>
2021-05-11 00:24:42 -0400

    Refined Stress Calculating
    Outputs a file with start and end frames for stress events.

--- Stress Events/main.py
+++ Stress Events/main.py
@@ -2,13 +2,14 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sb
 
-data = 'Summary of HARD26K_BINARY.csv'
+data = 'AnalyzeParticles_TIGHT.csv'
 df = pd.read_csv(data)
 df.drop(1, axis=0, inplace=True)
 slice_col = df['Slice']
 count_col = df['Count']
 stress_col = []
 stress_idx = []
+stress_idx_final = []
 total_stress = 0
 
 for idx, val in enumerate(count_col):
@@ -16,7 +17,7 @@ for idx, val in enumerate(count_col):
         print("Stress Event at: ", slice_col[idx])
         stress_col.append(2)
         total_stress += 1
-    elif val == 1:
+    elif val <= 4:
         print("Stress Event at: ", slice_col[idx])
         stress_col.append(1)
         total_stress += 1
@@ -36,6 +37,20 @@ for idx, val in enumerate(stress_col):
                 stress_col[idx] = 0
                 total_stress -= 1
 
+c = 0
+for idx, val in enumerate(stress_idx):
+    if idx != 0:
+        if val == stress_idx[idx-1]+1:
+            c += 1
+        else:
+            print("Consecutive: ", c, " at ", stress_idx[idx-c])
+            if c > 30:
+                stress_idx_final.append(stress_idx[idx-c])
+                stress_idx_final.append(stress_idx[idx-1])
+            c = 0
+
+print(stress_idx_final)
+
 print("\nRefined Total Length of Stress Events: ", total_stress)
 print("\nIndices of Stress Events: \n", stress_idx)
 
@@ -46,7 +61,7 @@ plt.xlabel("Slice")
 plt.show()
 
 output = open("StressEvents.txt", "w")
-output.write(str(stress_idx))
+output.write(', '.join(str(n) for n in stress_idx_final))
 output.close()
 print("\nSuccessfully written to output.")
 
